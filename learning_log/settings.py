3739596dcf568a959146b59d9c1f0ab954e349e4
rakshithgoud <117160104+Rakshith0405@users.noreply.github.com>
2025-03-17 15:48:18 +0000

    Updated code from Codespace

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-bh-1#+fq#lij=!k&g%60mdoivms3n0^5*lhstghlzms3i+mjv)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
